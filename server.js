46cd6804505c4737bc0243240992a62b67f9c08d
Nayan Desai <nayan@winspiretech.co>
2025-05-14 10:59:47 +0530

    local Commit

--- server.js
+++ server.js
@@ -2,9 +2,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-<<<<<<< HEAD
   console.log(`Server running on http://localhost:${PORT}`);
-=======
-    console.log(`Server running on http://localhost:${PORT}`);
->>>>>>> b71b593c19b495cbe0177161de4938a098073f89
 });
