ad194cbcd7dee27c293b444070eb45e353ace778
harshavardhana-winspire <harshavardhana.m@winspiretech.co>
2025-05-21 14:58:13 +0530

    Getting admins and changing role

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -439,7 +439,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "D:\\logiglo_backend_2.0\\generated\\prisma",
+      "value": "E:\\Logiglo\\logiglo_backend_2.0\\generated\\prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -461,7 +461,7 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "D:\\logiglo_backend_2.0\\prisma\\schema.prisma",
+    "sourceFilePath": "E:\\Logiglo\\logiglo_backend_2.0\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
@@ -475,11 +475,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://logiglo:root123@localhost:5432/logiglo_db?schema=public"
+        "value": null
       }
     }
   },
