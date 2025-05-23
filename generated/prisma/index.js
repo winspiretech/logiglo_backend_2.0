3b28d450fca529365ba05bb9308981ae5e02bf8a
Birender-Winspire <birender@winspiretech.co>
2025-05-23 15:08:14 +0530

    add controller in education

--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -466,7 +466,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../.env",
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
