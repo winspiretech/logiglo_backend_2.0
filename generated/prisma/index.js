7205c15dd2c1802e37638e12966413abd00feb0a
harshavardhana-winspire <harshavardhana.m@winspiretech.co>
2025-05-26 12:05:01 +0530

    Events schema changes done 2

--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -430,7 +430,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "D:\\Logiglo\\logiglo_backend_2.0\\generated\\prisma",
+      "value": "/logiglo_backend_2.0/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -439,7 +439,7 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "windows",
+        "value": "linux-musl-openssl-3.0.x",
         "native": true
       },
       {
@@ -452,7 +452,7 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "D:\\Logiglo\\logiglo_backend_2.0\\prisma\\schema.prisma",
+    "sourceFilePath": "/logiglo_backend_2.0/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
@@ -466,12 +466,11 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": null
+        "value": "postgresql://logiglo:root123@postgres:5432/logiglo_db?schema=public"
       }
     }
   },
@@ -513,6 +512,10 @@ warnEnvConflicts({
 const PrismaClient = getPrismaClient(config)
 exports.PrismaClient = PrismaClient
 Object.assign(exports, Prisma)
+
+// file annotations for bundling tools to include these files
+path.join(__dirname, "libquery_engine-linux-musl-openssl-3.0.x.so.node");
+path.join(process.cwd(), "generated/prisma/libquery_engine-linux-musl-openssl-3.0.x.so.node")
 
 // file annotations for bundling tools to include these files
 path.join(__dirname, "query_engine-windows.dll.node");
