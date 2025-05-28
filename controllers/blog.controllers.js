ff61e00ea6bb845464a6de0e70cc89a859a21871
harshavardhana-winspire <harshavardhana.m@winspiretech.co>
2025-05-28 17:48:01 +0530

    upadated blog controller

--- controllers/blog.controllers.js
+++ controllers/blog.controllers.js
@@ -84,7 +84,10 @@ const getAdminsBlogs = async (req, res) => {
     }
     const adminsBlogs = await prisma.blog.findMany({
       where: {
-        id: id,
+        authorId: id,
+      },
+      include: {
+        category: true,
       },
     });
     if (!adminsBlogs) {
@@ -113,7 +116,11 @@ const getAdminsBlogs = async (req, res) => {
 
 const getAllBlogs = async (req, res) => {
   try {
-    const allBlogs = await prisma.blog.findMany();
+    const allBlogs = await prisma.blog.findMany({
+      include: {
+        category: true,
+      }
+    });
     if (!allBlogs) {
       throw new ApiError(500, 'Something went wrong while fetching blogs');
     }
@@ -145,6 +152,9 @@ const getBlog = async (req, res) => {
       where: {
         id,
       },
+      include: {
+        category: true,
+      },
     });
     if (!blog) {
       throw new ApiError(500, 'Something went wrong while fetching blog');
@@ -170,36 +180,69 @@ const getBlog = async (req, res) => {
 const editBlog = async (req, res) => {
   try {
     const { id } = req.params;
+    const { categoryName, description, ...rest } = req.body;
+
     if (!id) {
-      throw new ApiError(400, 'Id is required', 'Id is required');
+      throw new ApiError(400, 'Validation Error', 'Blog ID is required');
     }
-    const { title, description } = req.body;
-    if (!title || !description) {
-      throw new ApiError(
-        400,
-        'All fields are required',
-        'Title or description is empty',
-      );
-    }
-    const updatedBlog = await prisma.blog.update({
+
+    const blog = await prisma.blog.findFirst({
       where: {
-        id: id,
-      },
+        id
+      }
+    });
+
+    if(blog?.authorId !== req?.user.id){
+      throw new ApiError(400, 'Validation Error', 'You are not the owner of the blog');
+    }
+
+    if (!description) {
+      throw new ApiError(400, 'Validation Error', 'Description is required');
+    }
+
+    const validateData = BlogSchema.safeParse(req.body);
+    if (!validateData.success) {
+      throw new ApiError(400, 'Validation Error', validateData.error.errors);
+    }
+
+    let category = null;
+    if (categoryName && categoryName.trim() !== '') {
+      const formattedName =
+        categoryName.trim().charAt(0).toUpperCase() +
+        categoryName.trim().slice(1).toLowerCase();
+
+      category = await prisma.blogCategory.findFirst({
+        where: {
+          name: {
+            equals: formattedName,
+            mode: 'insensitive',
+          },
+        },
+      });
+
+      if (!category) {
+        category = await prisma.blogCategory.create({
+          data: { name: formattedName },
+        });
+      }
+    }
+
+    const updatedBlog = await prisma.blog.update({
+      where: { id },
       data: {
-        title,
+        ...rest,
         description,
-        ...req.body,
+        categoryId: category?.id,
       },
     });
+
     res
       .status(200)
       .json(new ApiResponse(200, updatedBlog, 'Blog updated successfully'));
   } catch (error) {
     if (error instanceof ApiError) {
-      // Custom ApiError, send it back to the client
       return res.status(error.statusCode).json(error);
     } else {
-      // Handle other types of errors
       return res
         .status(500)
         .json(
