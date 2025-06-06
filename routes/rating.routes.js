9786199b9b0eed3dc9428f378d751d391a3a9b20
harshavardhana-winspire <harshavardhana.m@winspiretech.co>
2025-06-06 10:15:17 +0530

    commit before pull

--- routes/rating.routes.js
+++ routes/rating.routes.js
@@ -4,9 +4,11 @@ const { isUserLoggedIn } = require('../middleware/isUserLoggedIn');
 const {
   createRating,
   getAllRatingsByCourseId,
+  getIndividualRatingById,
 } = require('../controllers/rating.controller');
 
 router.post('/create/:courseId', isUserLoggedIn, createRating);
 router.get('/:courseId', getAllRatingsByCourseId);
+router.get('/user/:courseId', isUserLoggedIn, getIndividualRatingById);
 
 module.exports = router;
