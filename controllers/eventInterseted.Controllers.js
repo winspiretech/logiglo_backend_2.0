b703186b5254a9bf6abac05555845f72b23fcb5c
harshavardhana-winspire <harshavardhana.m@winspiretech.co>
2025-06-12 15:02:35 +0530

    Event interest by id created

--- controllers/eventInterseted.Controllers.js
+++ controllers/eventInterseted.Controllers.js
@@ -1,3 +1,4 @@
+const { tr } = require('zod/v4/locales');
 const prisma = require('../models/prismaClient');
 const { ApiError } = require('../utils/ApiError');
 const { ApiResponse } = require('../utils/ApiResponse');
@@ -92,4 +93,47 @@ const getAllEventIntrested = async (req, res) => {
   }
 };
 
-module.exports = { createEventInterset, getAllEventIntrested };
+const getInterestedDataByEventId = async (req,res) => {
+  try {
+    const { eventId } = req.params;
+    if (!eventId) {
+      throw new ApiError(404, 'Event Id required', 'Please send the Event Id');
+    }
+    const eventInterestedList = await prisma.eventIntrested.findMany({
+      where: {
+        eventId: eventId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+    if (!eventInterestedList) {
+      throw new ApiError(
+        404,
+        'No event interested found',
+        'No event interested data available for this event',
+      );
+    }
+    res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          eventInterestedList,
+          'Event interested list fetched successfully',
+        ),
+      );
+  } catch (error) {
+    if (error instanceof ApiError) {
+      return res.status(error.statusCode).json(error);
+    } else {
+      return res
+        .status(500)
+        .json(
+          new ApiError(500, 'Internal server error', error.message || null),
+        );
+    }
+  }
+}
+
+module.exports = { createEventInterset, getAllEventIntrested, getInterestedDataByEventId };
