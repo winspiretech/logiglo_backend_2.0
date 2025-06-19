f8e5c093890dcde916f4a4f31e19692540b0fb7a
Birender-Winspire <birender@winspiretech.co>
2025-06-19 16:42:32 +0530

    send user data after verification

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -341,7 +341,9 @@ const otpVerification = async (req, res, next) => {
 
     res
       .status(200)
-      .json(new ApiResponse(200, token, userDetails, 'User logged in successfully'));
+      .json(
+        new ApiResponse(200, {token, userDetails}, 'User logged in successfully'),
+      );
   } catch (error) {
     console.log(error.message || 'Something went wrong in OTP verification');
     if (error instanceof ApiError) {
