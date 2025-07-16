92dfd0f6dac2f4bf1ffc5258d3a972521496a2d3
harshavardhana-winspire <harshavardhana.m@winspiretech.co>
2025-07-16 15:23:16 +0530

    Refactor socket initialization and add user online status management

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
+// const app = require('./app');
+// const { archiveOldBlogs } = require('./jobs/archiveOldBlogs.job');
+// const { archiveOldEvents } = require('./jobs/archiveOldEvents.job');
+// const PORT = process.env.PORT || 3000;
+
+// archiveOldBlogs();
+// archiveOldEvents();
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+const http = require('http');
 const app = require('./app');
+const { initSocket } = require('./socket/socket'); 
 const { archiveOldBlogs } = require('./jobs/archiveOldBlogs.job');
 const { archiveOldEvents } = require('./jobs/archiveOldEvents.job');
+
 const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+initSocket(server);
 
 archiveOldBlogs();
 archiveOldEvents();
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
