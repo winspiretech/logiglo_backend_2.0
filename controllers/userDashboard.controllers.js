ee46c3de907cfcd4ebd7f4dfa552a06e345e3cef
harshavardhana-winspire <harshavardhana.m@winspiretech.co>
2025-07-21 10:55:49 +0530

    Refactor upcoming events logic to improve date handling and response structure

--- controllers/userDashboard.controllers.js
+++ controllers/userDashboard.controllers.js
@@ -64,47 +64,41 @@ const upcomingEventsInGiveDays = async (req, res) => {
   try {
     const { days = 3 } = req.query;
     const today = new Date();
-    const givenDaysLater = new Date();
-    givenDaysLater.setDate(today.getDate() + days);
+
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+    tomorrow.setHours(0, 0, 0, 0);
+
+    const givenDaysLater = new Date(tomorrow);
+    givenDaysLater.setDate(tomorrow.getDate() + parseInt(days));
+    givenDaysLater.setHours(23, 59, 59, 999);
 
     const upcomingEvents = await prisma.event.findMany({
       where: {
         startDate: {
-          gte: today,
+          gte: tomorrow,
           lte: givenDaysLater,
         },
       },
+      orderBy: {
+        startDate: 'asc',
+      },
     });
 
-    if (!upcomingEvents) {
-      throw new ApiError(
-        500,
-        'Internal server error',
-        'Something went wrong while fetching upcoming events',
-      );
-    }
-
     res
-      .status(201)
+      .status(200)
       .json(
         new ApiResponse(
-          201,
+          200,
           { upcomingEvents, totalUpcomingEvents: upcomingEvents.length },
-          'Upcoming events fetched successfully',
+          `Events from ${tomorrow.toISOString()} to ${givenDaysLater.toISOString()}`,
         ),
       );
   } catch (error) {
-    console.error(error.message || 'Error fetching user profile');
-
-    if (error instanceof ApiError) {
-      return res.status(error.statusCode).json(error);
-    } else {
-      return res
-        .status(500)
-        .json(
-          new ApiError(500, 'Internal server error', error.message || null),
-        );
-    }
+    console.error(error.message || 'Error fetching upcoming events');
+    return res
+      .status(500)
+      .json(new ApiError(500, 'Internal server error', error.message || null));
   }
 };
 
