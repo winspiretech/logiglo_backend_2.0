4417c2aeb37e546abde3268e02dceb548251b70f
vinay-winspire <vinay.v@winspiretech.co>
2026-04-01 16:20:02 +0530

    careers added

---
+++ routes/siteSettings.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const {
+  test,
+  getCareersIframeUrl,
+  updateCareersIframeUrl,
+} = require('../controllers/siteSettings.controllers');
+const isAdmin = require('../middleware/isAdmin');
+
+router.get('/test', test);
+router.get('/careers-iframe', getCareersIframeUrl);       // public - user frontend uses this
+router.patch('/careers-iframe', isAdmin, updateCareersIframeUrl); // admin only
+
+module.exports = router;
